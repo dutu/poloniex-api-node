afcd49e7c005bd935ea11bcb95c47d0345aa7634
dutu <adrian.clinciu@outlook.com>
2017-05-08 12:33:55 +0300

    add tests

---
+++ test/tests.js
@@ -0,0 +1,104 @@
+
+const expect = require('chai').expect;
+const Poloniex = require('../lib/poloniex.js');
+
+describe('Unit Test', function () {
+  describe("Constructor", function () {
+    it('should create a new instance', function () {
+      let poloniex = new Poloniex();
+      expect(poloniex).to.be.an.instanceOf(Poloniex);
+    });
+  });
+});
+
+describe("Integration Test", function () {
+  describe('returnTicker', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnTicker((error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('object');
+        done();
+      });
+    });
+  });
+  describe('return24Volume', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.return24Volume((error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('object');
+        done();
+      });
+    });
+  });
+  describe('returnOrderBook', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnOrderBook('BTC_XMR', null, (error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('object');
+        done();
+      });
+    });
+  });
+  describe('returnTradeHistory', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnTradeHistory('BTC_XMR', null, null, (error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('array');
+        done();
+      });
+    });
+  });
+  describe('returnChartData', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      let end = Date.now();
+      let start = end - 1000 * 60 * 60;
+      let period = 300;
+      poloniex.returnChartData('BTC_XMR', period, start, end, (error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('array');
+        done();
+      });
+    });
+  });
+  describe('returnCurrencies', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnCurrencies((error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('object');
+        done();
+      });
+    });
+  });
+  describe('returnLoanOrders', function () {
+    it('should return data', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnLoanOrders('BTC', null, (error, response) => {
+        expect(error).not.to.be.an.instanceOf(Error);
+        expect(response).to.be.an('object');
+        done();
+      });
+    });
+  });
+  describe('Trading API Methods', function () {
+    it('should require API key and secret', function (done) {
+      let poloniex = new Poloniex();
+      poloniex.returnBalances((error, response) => {
+        expect(error).to.be.an.instanceOf(Error);
+        done();
+      });
+    });
+    it('should return error when invalid API key/secret pair', function (done) {
+      let poloniex = new Poloniex('invalid key', 'invalid secret');
+      poloniex.returnBalances((error, response) => {
+        expect(error).to.be.an.instanceOf(Error);
+        done();
+      });
+    });
+  });
+});
