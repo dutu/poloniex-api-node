61eafda37d05d55f2f7512a31a44a31c9410e74e
dutu <dutu@protonmail.com>
2023-03-20 11:51:02 +0200

    update dependencies

--- .pnp.cjs
+++ .pnp.cjs
@@ -32,7 +32,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["chai", "npm:4.3.7"],\
             ["crypto-js", "npm:4.1.1"],\
             ["debug", "virtual:b86a9fb34323a98c6519528ed55faa0d9b44ca8879307c0b29aa384bde47ff59a7d0c9051b31246f14521dfb71ba3c5d6d0b35c29fffc17bf875aa6ad977d9e8#npm:4.3.4"],\
-            ["forever-websocket", "npm:0.3.0"],\
+            ["forever-websocket", "npm:0.3.1"],\
             ["mocha", "npm:10.2.0"]\
           ],\
           "linkType": "SOFT"\
@@ -667,12 +667,12 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["forever-websocket", [\
-        ["npm:0.3.0", {\
-          "packageLocation": "./.yarn/cache/forever-websocket-npm-0.3.0-8a57372994-bacf926761.zip/node_modules/forever-websocket/",\
+        ["npm:0.3.1", {\
+          "packageLocation": "./.yarn/cache/forever-websocket-npm-0.3.1-8b009ed39a-e45050ce6a.zip/node_modules/forever-websocket/",\
           "packageDependencies": [\
-            ["forever-websocket", "npm:0.3.0"],\
+            ["forever-websocket", "npm:0.3.1"],\
             ["eventemitter3", "npm:5.0.0"],\
-            ["ws", "virtual:8a57372994083d53e7a7a13a19d83e48499e988f06fe4f9c3bb80ca01f9474d90ca6ae27d9fc1b719272ac58fb4097cdeb4239019964c812b99e3e0d23532551#npm:8.13.0"]\
+            ["ws", "virtual:8b009ed39af44637e6ecfff9ae23f6010e5f1f28e575973e924b0f46bb719348dd8f83bbe987565882945fd3bd4a6d04d793d007a30372202f17a1448577d0c9#npm:8.13.0"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -1448,7 +1448,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["chai", "npm:4.3.7"],\
             ["crypto-js", "npm:4.1.1"],\
             ["debug", "virtual:b86a9fb34323a98c6519528ed55faa0d9b44ca8879307c0b29aa384bde47ff59a7d0c9051b31246f14521dfb71ba3c5d6d0b35c29fffc17bf875aa6ad977d9e8#npm:4.3.4"],\
-            ["forever-websocket", "npm:0.3.0"],\
+            ["forever-websocket", "npm:0.3.1"],\
             ["mocha", "npm:10.2.0"]\
           ],\
           "linkType": "SOFT"\
@@ -1834,10 +1834,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:8a57372994083d53e7a7a13a19d83e48499e988f06fe4f9c3bb80ca01f9474d90ca6ae27d9fc1b719272ac58fb4097cdeb4239019964c812b99e3e0d23532551#npm:8.13.0", {\
-          "packageLocation": "./.yarn/__virtual__/ws-virtual-d0799f6516/0/cache/ws-npm-8.13.0-26ffa3016a-53e991bbf9.zip/node_modules/ws/",\
+        ["virtual:8b009ed39af44637e6ecfff9ae23f6010e5f1f28e575973e924b0f46bb719348dd8f83bbe987565882945fd3bd4a6d04d793d007a30372202f17a1448577d0c9#npm:8.13.0", {\
+          "packageLocation": "./.yarn/__virtual__/ws-virtual-328d0496ce/0/cache/ws-npm-8.13.0-26ffa3016a-53e991bbf9.zip/node_modules/ws/",\
           "packageDependencies": [\
-            ["ws", "virtual:8a57372994083d53e7a7a13a19d83e48499e988f06fe4f9c3bb80ca01f9474d90ca6ae27d9fc1b719272ac58fb4097cdeb4239019964c812b99e3e0d23532551#npm:8.13.0"],\
+            ["ws", "virtual:8b009ed39af44637e6ecfff9ae23f6010e5f1f28e575973e924b0f46bb719348dd8f83bbe987565882945fd3bd4a6d04d793d007a30372202f17a1448577d0c9#npm:8.13.0"],\
             ["@types/bufferutil", null],\
             ["@types/utf-8-validate", null],\
             ["bufferutil", null],\
@@ -3418,7 +3418,7 @@ const DEFAULT_COMPRESSION_LEVEL = `mixed`;
 function toUnixTimestamp(time) {
   if (typeof time === `string` && String(+time) === time)
     return +time;
-  if (Number.isFinite(time)) {
+  if (typeof time === `number` && Number.isFinite(time)) {
     if (time < 0) {
       return Date.now() / 1e3;
     } else {
@@ -4000,7 +4000,7 @@ class ZipFS extends BasePortableFakeFS {
       resolvedP = ppath.resolve(parentP, ppath.basename(resolvedP));
       if (!resolveLastComponent || this.symlinkCount === 0)
         break;
-      const index = this.libzip.name.locate(this.zip, resolvedP.slice(1));
+      const index = this.libzip.name.locate(this.zip, resolvedP.slice(1), 0);
       if (index === -1)
         break;
       if (this.isSymbolicLink(index)) {
@@ -4025,7 +4025,7 @@ class ZipFS extends BasePortableFakeFS {
   allocateUnattachedSource(content) {
     const error = this.libzip.struct.errorS();
     const { buffer, byteLength } = this.allocateBuffer(content);
-    const source = this.libzip.source.fromUnattachedBuffer(buffer, byteLength, 0, true, error);
+    const source = this.libzip.source.fromUnattachedBuffer(buffer, byteLength, 0, 1, error);
     if (source === 0) {
       this.libzip.free(error);
       throw this.makeLibzipError(error);
@@ -4034,7 +4034,7 @@ class ZipFS extends BasePortableFakeFS {
   }
   allocateSource(content) {
     const { buffer, byteLength } = this.allocateBuffer(content);
-    const source = this.libzip.source.fromBuffer(this.zip, buffer, byteLength, 0, true);
+    const source = this.libzip.source.fromBuffer(this.zip, buffer, byteLength, 0, 1);
     if (source === 0) {
       this.libzip.free(buffer);
       throw this.makeLibzipError(this.libzip.getError(this.zip));
@@ -10468,7 +10468,7 @@ const makeInterface = (libzip) => ({
     locate: libzip.cwrap(`zip_name_locate`, `number`, [`number`, `string`, `number`])
   },
   source: {
-    fromUnattachedBuffer: libzip.cwrap(`zip_source_buffer_create`, `number`, [`number`, `number`, `number`, `number`]),
+    fromUnattachedBuffer: libzip.cwrap(`zip_source_buffer_create`, `number`, [`number`, ...number64, `number`, `number`]),
     fromBuffer: libzip.cwrap(`zip_source_buffer`, `number`, [`number`, `number`, ...number64, `number`]),
     free: libzip.cwrap(`zip_source_free`, null, [`number`]),
     keep: libzip.cwrap(`zip_source_keep`, null, [`number`]),
@@ -12365,7 +12365,7 @@ ${candidates.map((candidate) => `Not found: ${getPathForDisplay(candidate)}
       conditions: (_a = opts2.conditions) != null ? _a : defaultExportsConditions,
       readFileSyncFn: tryReadFile
     });
-    if (resolved instanceof URL) {
+    if (resolved instanceof url.URL) {
       return resolveUnqualified(npath.toPortablePath(url.fileURLToPath(resolved)), { extensions: opts2.extensions });
     } else {
       if (resolved.startsWith(`#`))
