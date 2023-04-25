1184da4beba926158982c4878fe728e0b70ecf3b
dutu <dutu@protonmail.com>
2023-04-25 15:58:21 +0300

    fix `main` entry

--- test/package.test.cjs
+++ test/package.test.cjs
@@ -3,7 +3,7 @@ const packageJSON = require('../package.json');
 
 describe('package.json', () => {
   it('should have the correct "main" entry for CommonJS', () => {
-    expect(packageJSON.main).to.equal('dist/cjs/poloniex.cjs');
+    expect(packageJSON.main).to.equal('./dist/cjs/poloniex.cjs');
   });
 
   it('should have the correct "module" entry for ESM', () => {
